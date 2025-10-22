74c9572070b8ed4b6c0d4a5a076cb6a17e8ed184
Sumiran Bastola <bastolasumi90@gmail.com>
2025-10-22 13:38:42 +0530

    backend added

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+const HOST = process.env.HOST || 'localhost';
+const PORT = process.env.PORT || 3000;
+
+(async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  
+  // Enable CORS for frontend
+  app.enableCors({
+    origin: true,
+    // credentials: true,
+  });
+
+  // await app.listen(PORT, HOST);
+  await app.listen(3000, "0.0.0.0", () => console.log("Server running"));
+
+  console.log(`Application is running on: http://${HOST}:${PORT}`);
+})();
