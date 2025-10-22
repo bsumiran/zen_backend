969f7d09aebcd548ad4bca453840e5cfad0587c5
Sumiran Bastola <bastolasumi90@gmail.com>
2025-10-22 13:50:28 +0530

    localhost removed

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-const HOST = process.env.HOST || 'localhost';
-const PORT = process.env.PORT || 3000;
+const HOST = process.env.HOST ;
+const PORT = process.env.PORT;
 
 (async function bootstrap() {
   const app = await NestFactory.create(AppModule);
