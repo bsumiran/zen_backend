979216714b862e7ed66b3ad077f1a3618ea2fdeb
Sumiran Bastola <bastolasumi90@gmail.com>
2025-10-22 13:41:43 +0530

    backend port changed

--- src/main.ts
+++ src/main.ts
@@ -13,8 +13,8 @@ const PORT = process.env.PORT || 3000;
     // credentials: true,
   });
 
-  // await app.listen(PORT, HOST);
-  await app.listen(3000, "0.0.0.0", () => console.log("Server running"));
+  await app.listen(PORT, HOST);
+  // await app.listen(3000, "0.0.0.0", () => console.log("Server running"));
 
   console.log(`Application is running on: http://${HOST}:${PORT}`);
 })();
